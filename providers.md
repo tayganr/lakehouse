d3c98594ffc3f39fd5e46ff417e1e4d2d8ac4566
Taygan Rifat <taygan@gmail.com>
2022-09-12 11:31:53 +0100

    code snippet

---
+++ providers.md
@@ -0,0 +1,42 @@
+# How to Register a Resource Provider
+
+[< Previous Module](./azurepass.md) - **[Home](./README.md)** - [Next Module >](./modules/module00.md)
+
+## :stopwatch: Estimated Duration
+
+10 minutes
+
+## :thinking: Prerequisites
+
+* An Azure subscription
+
+## :loudspeaker: Introduction
+
+TBC
+
+## Initialize Cloud Shell
+
+1. In the Azure Portal, click the **Cloud Shell** icon.
+1. Set the **Subscription** to target Azure Subscription (e.g. `Azure Pass - Sponsorship`).
+1. Click **Create storage**
+
+    ![ALT](./images/providers/001.png)
+
+
+## Initialize Cloud Shell
+
+1. Hover your mouse over the code snippet below and click **Copy** button.
+
+```powershell
+$uri = "https://raw.githubusercontent.com/tayganr/lakehouse/main/template/lakehouselab.ps1"
+Invoke-WebRequest $uri -OutFile "preDeploymentScript.ps1"
+./preDeploymentScript.ps1
+```
+
+2. Paste the code snippet into Cloud Shell.
+
+## :tada: Summary
+
+You have successfully registered all the required resource providers.
+
+[Continue >](./modules/module00.md)
---
+++ template/lakehouselab.ps1
@@ -0,0 +1,37 @@
+﻿Write-Host "`n[INFO] Getting a list of Azure subsciptions that the current account can access..."
+$subscriptions = Get-AzSubscription
+
+$counter = 0
+Write-Host "`n------------------------------------------`n         YOUR AZURE SUBSCRIPTIONS         `n------------------------------------------"
+ForEach ($x in $subscriptions) {
+    if ($x.State -eq "Enabled") {
+        $counter++
+        Write-Host "[$($counter)] ($($x.Id)) $($x.Name)"
+    }
+}
+$selectSub = Read-Host "`nPlease select an Azure subscrpition by specifying a number between 1 and $($counter)"
+$targetSubscription = $subscriptions[$selectSub]
+
+Write-Host "`n[INFO] Setting the Azure context to: $($targetSubscription.Name)"
+$setContext = Set-AzContext -Subscription $targetSubscription.Id
+
+$requiredResourceProviers = @("Microsoft.Authorization","Microsoft.EventGrid","Microsoft.Sql","Microsoft.Storage","Microsoft.Synapse","Microsoft.DataFactory")
+Write-Host "[INFO] Getting all resource providers registered with the current subscription..."
+$registeredResourceProviders = Get-AzResourceProvider
+
+Write-Host "[INFO] Registering missing resource providers..."
+$isMissing = $false
+ForEach ($requiredResourceProvider in $requiredResourceProviers) {
+    if ($requiredResourceProvider -in $registeredResourceProviders.ProviderNamespace) {
+        continue
+    } else {
+        $isMissing = $true
+        Register-AzResourceProvider -ProviderNamespace $requiredResourceProvider
+    }
+}
+
+if ($isMissing) {
+    Write-Host "[INFO] Missing resource providers are now registering. To check the registration status, navigate to Azure Portal > Subscriptions > YOUR_AZURE_SUBSCRIPTION > Resouce Providers."
+} else {
+    Write-Host "[INFO] All required resource providers are registered.`n"
+}
